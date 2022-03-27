56399b743952bc262b0cb589a17c8ee2fb456df0
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-27 17:11:30 +0200

    [Fix] Main page content

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -80,8 +80,8 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm CodeBucks.</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h3>I'm Victor.</h3>
+                    <h6>I study Data Science and Data Engineering in EPITA.</h6>
                 </Text>
             </SubBox>
             <SubBox>
--- src/components/Main.js
+++ src/components/Main.js
@@ -145,7 +145,7 @@ const Main = () => {
                 <span>click here</span>
             </Center>
 
-            <Contact target="_blank" to={{pathname:"mailto:codebucks27@gmail.com"}}>
+            <Contact target="_blank" to={{pathname:"mailto:victor.simonin@epita.fr"}}>
                 <motion.h2
                 initial={{
                     y:-200,
