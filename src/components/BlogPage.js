55efa651dad0b735834b4eb6530d4e2b67a0f4f0
Victor Simonin <victor.simonin@epita.fr>
2022-04-25 18:42:19 +0100

    [Add] Responsive for blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,100 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
-import LogoComponent from '../subComponents/LogoComponent'
-import SocialIcons  from '../subComponents/SocialIcons'
-import PowerButton  from '../subComponents/PowerButton'
+import React from "react";
+import styled from "styled-components";
+import { lazy, Suspense, useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-import {Blogs} from '../data/BlogData';
-import BlogComponent from './BlogComponent'
-import AnchorComponent from '../subComponents/Anchor'
-import BigTitle from "../subComponents/BigTitle"
-import { motion } from 'framer-motion'
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg";
+import { Blogs } from "../data/BlogData";
 
+import BlogComponent from "./BlogComponent";
+import Loading from "../subComponents/Loading";
+import { mediaQueries } from "./Themes";
+
+const AnchorComponent = lazy(() => import("../subComponents/Anchor"));
+const SocialIcons = lazy(() => import("../subComponents/SocialIcons"));
+const PowerButton = lazy(() => import("../subComponents/PowerButton"));
+const LogoComponent = lazy(() => import("../subComponents/LogoComponent"));
+const BigTitle = lazy(() => import("../subComponents/BigTitle"));
 
 const MainContainer = styled(motion.div)`
-background-image: url(${img});
-background-size: cover;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-position: center;
-`
-const Container = styled.div`
-background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
-width: 100%;
-height:auto;
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+`;
 
-position: relative;
-padding-bottom: 5rem;
-`
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+
+  //width:100vw;
+  width: 100%;
+  height: auto;
+  position: relative;
+  padding-bottom: 5rem;
+`;
 
 const Center = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-padding-top: 10rem;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
 
-const Grid = styled.div`
-display: grid;
-grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
-grid-gap: calc(1rem + 2vw);
-`
+  ${mediaQueries(30)`
+    padding-top: 7rem;
+    
+  
+  `};
+`;
 
-// Framer-motion config
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+
+  grid-gap: calc(1rem + 2vw);
+
+  ${mediaQueries(50)`
+    grid-template-columns: 100%;
+
+    
+  
+  `};
+`;
+
 const container = {
-
-    hidden: {opacity:0},
-    show: {
-      opacity:1,
-  
-      transition:{
-        staggerChildren: 0.5,
-        duration: 0.5,
-      }
-    }
-  
-  }
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+      duration: 0.5,
+    },
+  },
+};
 
 const BlogPage = () => {
+  const [number, setNumber] = useState(0);
 
-    const [numbers, setNumbers] = useState(0);
-
-    useEffect(() => {
-        let num = (window.innerHeight - 70)/30;
-        setNumbers(parseInt(num));
-    }, [])
-
-
-    return (
-        <MainContainer
+  useEffect(() => {
+    let num = (window.innerHeight - 70) / 30;
+    setNumber(parseInt(num));
+  }, []);
+  return (
+    <Suspense fallback={<Loading />}>
+      <MainContainer
         variants={container}
-        initial='hidden'
-        animate='show'
-        exit={{
-            opacity:0, transition:{duration: 0.5}
-        }}
-        >
-            <Container>
-                <LogoComponent />
-                <PowerButton />
-                <SocialIcons />
-                <AnchorComponent number={numbers}/>
-<Center>
-<Grid>
+        initial="hidden"
+        animate="show"
+        exit={{ opacity: 0, transition: { duration: 0.5 } }}
+      >
+        <Container>
+          <LogoComponent />
 
-{
-    Blogs.map(blog => {
-        return <BlogComponent key={blog.id} blog={blog} />
-    })
-}
-</Grid>
+          <PowerButton />
 
-</Center>
-<BigTitle text="BLOG" top="5rem" left="5rem" />
-            </Container>
-        </MainContainer>
-    )
-}
+          <SocialIcons />
+          <AnchorComponent number={number} />
 
-export default BlogPage
+          <Center>
+            <Grid variants={container} initial="hidden" animate="show">
+              {Blogs.map((blog) => (
+                <BlogComponent key={blog.id} blog={blog} />
+              ))}
+            </Grid>
+          </Center>
+
+          <BigTitle text="BLOG" top="5rem" left="5rem" />
+        </Container>
+      </MainContainer>
+    </Suspense>
+  );
+};
+
+export default BlogPage;
