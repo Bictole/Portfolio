a86dbae315d7bbd7d0af3dfaf68da8d3e655bb25
Victor Simonin <victor.simonin@epita.fr>
2022-04-27 17:21:18 +0100

    [Add] Work responsiveness and rotating python logo fix

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,115 +1,154 @@
-import React, { useEffect, useRef } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import {DarkTheme} from './Themes';
-import {motion} from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
+import { useEffect, useRef, lazy, Suspense } from "react";
 
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
+import styled, { ThemeProvider } from "styled-components";
 
+import { Python } from "./AllSvgs";
 import { Work } from "../data/WorkData";
-import Card from '../subComponents/Card';
-import { Python } from './AllSvgs';
-import BigTitlte from '../subComponents/BigTitle';
+import { DarkTheme, mediaQueries } from "./Themes";
 
-const Box = styled.div`
-background-color: ${props => props.theme.body};
+import Card from "../subComponents/Card";
+import Loading from "../subComponents/Loading";
 
-height:400vh;
-position: relative;
-display: flex;
-align-items: center;
-`
+const SocialIcons = lazy(() => import("../subComponents/SocialIcons"));
+const PowerButton = lazy(() => import("../subComponents/PowerButton"));
+const LogoComponent = lazy(() => import("../subComponents/LogoComponent"));
+const BigTitle = lazy(() => import("../subComponents/BigTitle"));
+
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  display: flex;
+  height: 400vh;
+`;
 
 const Main = styled(motion.ul)`
-position: fixed;
-top: 12rem;
-left:calc(10rem + 15vw);
-height: 40vh;
-display: flex;
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
 
-color:white;
-`
+  height: 40vh;
+  /* height:200vh; */
+  //border:1px solid white;
+
+  display: flex;
+
+  ${mediaQueries(50)`
+        
+        
+        left:calc(8rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+  top: 30%;
+        
+        left:calc(6rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+        
+        left:calc(2rem + 15vw);
+
+  `};
+  ${mediaQueries(25)`
+        
+        left:calc(1rem + 15vw);
+
+  `};
+`;
 
 const Rotate = styled.span`
-display:block;
-position: fixed;
-right:1rem;
-bottom: 1rem;
-width: 80px;
-height: 80px;
-z-index:1;
-`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
 
+  z-index: 1;
+  ${mediaQueries(40)`
+     width:60px;
+         height:60px;   
+       svg{
+         width:60px;
+         height:60px;
+       }
 
-// Framer-motion Configuration
+  `};
+  ${mediaQueries(25)`
+        width:50px;
+         height:50px;
+        svg{
+         width:50px;
+         height:50px;
+       }
+
+  `};
+`;
 const container = {
-
-  hidden: {opacity:0},
+  hidden: { opacity: 0 },
   show: {
-    opacity:1,
+    opacity: 1,
 
-    transition:{
+    transition: {
       staggerChildren: 0.5,
       duration: 0.5,
-    }
-  }
-
-}
+    },
+  },
+};
 
 const WorkPage = () => {
+  const ref = useRef(null);
 
-    const ref = useRef(null);
-    const python = useRef(null);
+  const python = useRef(null);
 
+  useEffect(() => {
+    let element = ref.current;
 
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`;
 
-    useEffect(() => {
-        let element = ref.current;
-       
-        
-        const rotate = () => {
-         
-         element.style.transform = `translateX(${-window.pageYOffset}px)`
-      
-         
-          //python.current.style.transform =
-          //  'rotate(' + -window.pageYOffset + 'deg)'
-        }
-    
-        window.addEventListener('scroll', rotate)
-        return () => {
-          window.removeEventListener('scroll', rotate);
-          
-        }
-      }, [])
+      return (python.current.style.transform =
+        "rotate(" + -window.pageYOffset + "deg)");
+    };
 
+    window.addEventListener("scroll", rotate);
+    return () => {
+      window.removeEventListener("scroll", rotate);
+    };
+  }, []);
 
-    return (
-        <ThemeProvider theme={DarkTheme}>
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Suspense fallback={<Loading />}>
+        <Box
+          key="work"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        >
+          <LogoComponent theme="dark" />
+          <PowerButton />
+          <SocialIcons theme="dark" />
 
-          <Box>
-
-            <LogoComponent theme='dark'/>
-            <SocialIcons theme='dark'/>
-            <PowerButton />
-
-            <Main ref={ref}   variants={container} initial='hidden' animate='show'  >
-            {
-              Work.map( d => 
+          <Main ref={ref} variants={container} initial="hidden" animate="show">
+            {Work.map((d) => (
               <Card key={d.id} data={d} />
-              )
-            }
-            </Main>
-            <Rotate ref={python}>
-              <Python width={80} height={80} fill={DarkTheme.text} />
-            </Rotate>
+            ))}
+          </Main>
 
-            <BigTitlte text="WORK" top='10%' right="20%" />
-          </Box>
+          <BigTitle text="WORK" top="10%" right="20%" />
 
-        </ThemeProvider>
-    )
-}
+          <Rotate ref={python}>
+            <Python width={80} height={80} fill={DarkTheme.text} />
+          </Rotate>
+        </Box>
+      </Suspense>
+    </ThemeProvider>
+  );
+};
 
-export default WorkPage
+export default WorkPage;
