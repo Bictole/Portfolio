69e816f085acbbc1c1454ace91ff280a2637bff5
Victor Simonin <victor.simonin@epita.fr>
2022-04-27 16:21:18 +0100

    [Add] Myskills responsive part

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,154 +1,210 @@
-import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import {lightTheme} from './Themes';
-import { Design, Develope} from './AllSvgs';
+import React from "react";
+import { motion } from "framer-motion";
+import styled, { ThemeProvider } from "styled-components";
+import { lazy, Suspense } from "react";
+import { lightTheme, mediaQueries } from "./Themes";
 
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import ParticleComponent from '../subComponents/ParticlesComponent';
-import BigTitle from '../subComponents/BigTitle'
+import { Design, Develope } from "./AllSvgs";
+import Loading from "../subComponents/Loading";
 
+//Components
+const SocialIcons = lazy(() => import("../subComponents/SocialIcons"));
+const LogoComponent = lazy(() => import("../subComponents/LogoComponent"));
+const ParticlesComponent = lazy(() =>
+  import("../subComponents/ParticlesComponent")
+);
+const BigTitle = lazy(() => import("../subComponents/BigTitle"));
 
-const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:120vh;
-position: relative;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-`
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 
-const Main = styled.div`
-border: 2px solid ${props => props.theme.text};
-color: ${props => props.theme.text};
-background-color: ${props => props.theme.body};
-padding: 2rem;
-width: 30vw;
-height: 80vh;
-z-index:3;
-line-height: 1.5;
-cursor: pointer;
+  ${mediaQueries(50)`
+            flex-direction:column;  
+            padding:8rem 0;
+height:auto;
+            &>*:nth-child(5){
+              margin-bottom:5rem;
+            }
+           
+  `};
+  ${mediaQueries(30)`
+           
+            &>*:nth-child(5){
+              margin-bottom:4rem;
+            }
+           
+  `};
+`;
 
-font-family: 'Ubuntu Mono',monospace;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
+const Main = styled(motion.div)`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
 
-&:hover{
-    color: ${props => props.theme.body};
-    background-color: ${props => props.theme.text};
-}
-`
+  ${mediaQueries(60)`
+            height: 55vh;
+  `};
+
+  ${mediaQueries(50)`
+              width: 50vw;
+              height: max-content;
+
+  `};
+
+  font-family: "Ubuntu Mono", monospace;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
 
 const Title = styled.h2`
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: calc(1em + 1vw);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
 
-${Main}:hover &{
-    &>*{
-        fill:${props => props.theme.body};
+  ${mediaQueries(60)`
+          font-size:calc(0.8em + 1vw);
+  `};
+
+  ${mediaQueries(50)`
+          font-size:calc(1em + 2vw);
+          margin-bottom:1rem;
+  `};
+
+  ${mediaQueries(30)`
+                      font-size:calc(1em + 1vw);
+  `};
+  ${mediaQueries(25)`
+                      font-size:calc(0.8em + 1vw);
+                      svg{
+                        width:30px;
+                        height:30px;
+                      }
+  `};
+
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
     }
-}
+  }
 
-&>*:first-child{
-margin-right: 1rem;
-}
-`
-
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
 const Description = styled.div`
-color: ${props => props.theme.text};
-font-size: calc(0.6em + 1vw);
-padding: 0.5rem 0;
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
 
-${Main}:hover &{
-   
-        color:${props => props.theme.body};
-    
-}
+  ${mediaQueries(50)`
+            font-size: calc(0.8em + 1vw);
 
-strong{
+  `};
+
+  ${mediaQueries(30)`
+                      font-size:calc(0.7em + 1vw);
+
+              
+
+  `};
+
+  ${mediaQueries(25)`
+                      font-size:calc(0.5em + 1vw);
+
+              
+
+  `};
+
+  strong {
     margin-bottom: 1rem;
     text-transform: uppercase;
-}
-ul,p{
+  }
+  ul,
+  p {
     margin-left: 2rem;
-}
-`
+  }
+`;
 
 const MySkillsPage = () => {
-    return (
-        <ThemeProvider theme={lightTheme}>
-            <Box>
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Suspense fallback={<Loading />}>
+        <Box
+          key="skills"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        >
+          <LogoComponent theme="light" />
+          <SocialIcons theme="light" />
+          <ParticlesComponent theme="light" />
 
-                <LogoComponent theme='light'/>
-                <SocialIcons theme='light'/>
-                <ParticleComponent theme='light' />
+          <Main>
+            <Title>
+              <Design width={40} height={40} /> Data Scientist
+            </Title>
+            <Description>
+            I love hacking problems and learning new knowledges !
+            </Description>
+            <Description>
+              <strong>I like to work on</strong> <br />
+              <ul>
+                <li>Machine Learning problems, i need more experience on them...</li>
+                <li>Mathematics, to be more comfortable on Convex optimizations or on Gradient Descent ! </li>
+              </ul>
+            </Description>
+            <Description>
+              <strong>Tools</strong> <br />
+              <ul>
+                <li>Python (Numpy, Pandas, Keras, Tensorflow, Sci-kit Learn), Scala, Spark</li>
+              </ul>
+            </Description>
+          </Main>
+          <Main>
+            <Title>
+              <Develope width={40} height={40} /> Software Engineer
+            </Title>
+            <Description>
+            I believe that a good Data Scientist/Engineer is a good Software Engineer with good practices !
+            </Description>
+            <Description>
+              <strong>Skills</strong> <br />
+              <p>
+                Python, Scala, C++, C, C#, Java, Ocaml, JS
+              </p>
+            </Description>
+            <Description>
+              <strong>Tools</strong> <br />
+              <p>VScode, Github, Gitlab, CI/CD</p>
+            </Description>
+          </Main>
+          <BigTitle text="Skills" top="80%" right="30%" />
+        </Box>
+      </Suspense>
+    </ThemeProvider>
+  );
+};
 
-                <Main>
-                    <Title>
-                        <Design width={40} height={40} /> Data Scientist
-                    </Title>
-                    
-                    <Description>
-                        I love hacking problems and learning new knowledges !
-                    </Description>
-                    
-                    <Description>
-                        <strong>I like to work on</strong>
-                        <ul>
-                            <li>
-                                Machine Learning problems, i need more experience on them...
-                            </li>
-                            <li>
-                                Mathematics, to be more comfortable on Convex optimizations or on Gradient Descent ! 
-                            </li>
-                        </ul>
-                    </Description>
-
-                    <Description>
-                        <strong>Tools</strong>
-                        <ul>
-                            <li>
-                                Python (Numpy, Pandas, Keras, Tensorflow, Sci-kit Learn), Scala, Spark
-                            </li>
-    
-                        </ul>
-                    </Description>
-                </Main>
-            
-                <Main>
-                    <Title>
-                        <Develope width={40} height={40} /> Software Engineer
-                    </Title>
-                    
-                    <Description>
-                        I believe that a good Data Scientist/Engineer is a good Software Engineer with good practices !  
-                    </Description>
-
-                    <Description>
-                        <strong>Skills</strong>
-                        <p>
-                            Python, Scala, C++, C, C#, Java, Ocaml, JS
-                        </p>
-                    </Description>
-                    
-                    <Description>
-                        <strong>Tools</strong>
-                        <p>
-                            VScode, Github, Gitlab, CI/CD
-                        </p>
-                    </Description>
-
-                </Main>
-
-                <BigTitle text="SKILLS" top="80%" right="30%" />
-
-            </Box>
-        </ThemeProvider>  
-    )
-}
-
-export default MySkillsPage
+export default MySkillsPage;
