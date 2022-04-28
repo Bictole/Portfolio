7db24d34aea94505045e06277152100d5e695ca5
Victor Simonin <victor.simonin@epita.fr>
2022-04-28 20:21:18 +0100

    [Add] Social Icons responsiveness

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -1,80 +1,118 @@
-import { motion } from 'framer-motion'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
+import React from 'react';
 import { Github, Linkedin, Kaggle } from '../components/AllSvgs'
-import {DarkTheme} from '../components/Themes'
+
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import { NavLink } from "react-router-dom";
+import { DarkTheme, mediaQueries } from "../components/Themes";
 
 const Icons = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
 
-position: fixed;
-bottom: 0;
-left: 2rem;
+  z-index: 3;
 
-z-index:3;
-
-&>*:not(:last-child){
+  & > *:not(:last-child) {
     margin: 0.5rem 0;
-}
 
-`
+    ${mediaQueries(20)`
+      margin: 0.3rem 0;
 
+
+  `};
+  }
+
+  ${mediaQueries(40)`
+        left: 1rem;
+        color: ${(props) => props.theme.body};
+        svg{
+            width:20px;
+            height:20px
+        } 
+  `};
+`;
 const Line = styled(motion.span)`
-width: 2px;
-height: 8rem;
-background-color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body  };
-`
-
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
 const SocialIcons = (props) => {
-    return (
-        <Icons>
-            <motion.div
-            initial={{transform:"scale(0)"}}
-            animate={{scale:[0,1,1.5,1]}}
-            transition={{type:'spring', duration:1, delay:1}}
-            >
-                <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://github.com/Bictole"}}>
-                    <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
-                </NavLink>
-            </motion.div>
-            <motion.div
-            initial={{transform:"scale(0)"}}
-            animate={{scale:[0,1,1.5,1]}}
-            transition={{type:'spring', duration:1, delay:1.2}}
-            >
-                <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://www.linkedin.com/in/victor-simonin/"}}>
-                    <Linkedin width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
-                </NavLink>
-            </motion.div>
-            <motion.div
-            initial={{transform:"scale(0)"}}
-            animate={{scale:[0,1,1.5,1]}}
-            transition={{type:'spring', duration:1, delay:1.4}}
-            >
-                <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://www.kaggle.com/victorsimonin"}}>
-                    <Kaggle width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
-                </NavLink>
-            </motion.div>
-            
-            <Line color={props.theme}
+  const mq = window.matchMedia("(max-width: 40em)").matches;
 
-initial={
-    {
-        height:0
-    }
-}
-animate={{
-    height: '8rem'
-}}
-transition={{
-    type:'spring', duration:1, delay:0.8
-}}
-            />
-        </Icons>
-    )
-}
+  return (
+    <Icons>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://github.com/Bictole" }}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={
+              props.theme === "dark" ? `${DarkTheme.text}` : `${DarkTheme.body}`
+            }
+          />
+        </NavLink>
+      </motion.div>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.2 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.linkedin.com/in/victor-simonin/" }}
+        >
+          <Linkedin
+            width={25}
+            height={25}
+            fill={
+              props.theme === "dark" ? `${DarkTheme.text}` : `${DarkTheme.body}`
+            }
+          />
+        </NavLink>
+      </motion.div>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.kaggle.com/victorsimonin" }}
+        >
+          <Kaggle
+            width={25}
+            height={25}
+            fill={
+              props.theme === "dark" ? `${DarkTheme.text}` : `${DarkTheme.body}`
+            }
+          />
+        </NavLink>
+      </motion.div>
 
-export default SocialIcons
+      <Line
+        initial={{ height: 0 }}
+        animate={{ height: mq ? "5rem" : "8rem" }}
+        color={props.theme}
+        transition={{ type: "spring", duration: 1, delay: 0.8 }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
