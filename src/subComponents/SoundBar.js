07cf2d49b6b6cffa69615e5f8ab0ed4daff5a600
Victor Simonin <victor.simonin@epita.fr>
2022-04-28 22:21:18 +0100

    [Add] Sound bar responsive

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -1,32 +1,41 @@
-import React, { useRef, useState } from 'react'
-import styled, { keyframes } from 'styled-components'
+import React from "react";
+import { useRef, useState } from "react";
+import styled, { keyframes } from "styled-components";
 
 import music from "../assets/audio/Mister_Magic.mp3"
+import { mediaQueries } from "../components/Themes";
 
 const Box = styled.div`
-display:flex;
-cursor:pointer;
-position:fixed;
-left:8rem;
-top:3rem;
-z-index:10;
+  display: flex;
+  cursor: pointer;
 
-&>*:nth-child(1){
+  position: fixed;
+  left: 8rem;
+  top: 3rem;
+  z-index: 10;
+
+  & > *:nth-child(1) {
     animation-delay: 0.2s;
-}
-&>*:nth-child(2){
+  }
+  & > *:nth-child(2) {
     animation-delay: 0.3s;
-}
-&>*:nth-child(3){
+  }
+  & > *:nth-child(3) {
     animation-delay: 0.4s;
-}
-&>*:nth-child(4){
+  }
+  & > *:nth-child(4) {
     animation-delay: 0.5s;
-}
-&>*:nth-child(5){
+  }
+  & > *:nth-child(5) {
     animation-delay: 0.8s;
-}
-`
+  }
+
+  ${mediaQueries(40)`
+      left:1rem;
+top:10rem;
+
+  `};
+`;
 
 const play = keyframes`
 0%{
@@ -38,44 +47,50 @@ const play = keyframes`
 100%{
     transform:scaleY(1);
 }
-`
-const Line = styled.span`
-background: ${props => props.theme.text};
-border: 1px solid ${props => props.theme.body};
+`;
 
-animation:${play} 1s ease infinite;
-animation-play-state: ${props => props.click ? "running" : "paused"};
-height: 1rem;
-width: 2px;
-margin:0 0.1rem
-`
+const Line = styled.span`
+  background: ${(props) => props.theme.text};
+  border: 1px solid ${(props) => props.theme.body};
+
+  animation: ${play} 1s ease infinite;
+  animation-play-state: ${(props) => (props.click ? "running" : "paused")};
+  height: 1rem;
+  width: 2px;
+  margin: 0 0.1rem;
+
+  ${mediaQueries(40)`
+      height:0.5rem;
+      width:1px;
+
+  `};
+`;
 
 const SoundBar = () => {
+  const [click, setClick] = useState(false);
 
-    const ref = useRef(null);
-    const [click, setClick] = useState(false);
-
-    const handleClick = () => {
-        setClick(!click);
-
-        if(!click){
-            ref.current.play();
-        }else{
-            ref.current.pause();
-        }
+  const handleClick = () => {
+    setClick(!click);
+    if (!click) {
+      ref.current.play();
+    } else {
+      ref.current.pause();
     }
-    return (
-        <Box onClick={() => handleClick()}>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
+  };
 
+  const ref = useRef(null);
 
-            <audio src={music} ref={ref}  loop />
-        </Box>
-    )
-}
+  return (
+    <Box onClick={() => handleClick()}>
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
 
-export default SoundBar
+      <audio src={music} ref={ref} loop />
+    </Box>
+  );
+};
+
+export default SoundBar;
