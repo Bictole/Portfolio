6b3e99b241577dd43ac5a20bc08847be3d7b854a
Victor Simonin <victor.simonin@epita.fr>
2022-11-23 20:34:40 +0100

    [Add] ChristmasList fix french translate

--- src/components/ChristmasList.js
+++ src/components/ChristmasList.js
@@ -20,7 +20,7 @@ const ChristmasList = () => {
         <h2>------</h2>
         <ul>
             <li>Un parfum (déja pris trop tard !)</li>
-            <li>Livre The Watcher 2, plutot pas la version poche <a href='https://livre.fnac.com/a12978611/The-Witcher-Sorceleur-Tome-2-Sorceleur-T2-L-Epee-de-la-providence-Andrzej-Sapkowski'>(celui-la par exemple)</a></li>
+            <li>Livre The Witcher 2 (Le sorceleur en francais), plutot pas la version poche <a href='https://livre.fnac.com/a12978611/The-Witcher-Sorceleur-Tome-2-Sorceleur-T2-L-Epee-de-la-providence-Andrzej-Sapkowski'>(celui-la par exemple)</a></li>
             <li>N'ayant pas beaucoup d'idée, je vais ajouter des éléments dans cette liste dans les jours a venir !</li>
         </ul>
     </Box>
