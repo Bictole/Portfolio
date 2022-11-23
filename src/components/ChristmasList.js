580413b32e4bdf73121a681bcb8de60025bef2c1
Victor Simonin <victor.simonin@epita.fr>
2022-11-23 13:53:32 +0100

    [Add] ChristmasList h2 space below title

--- src/components/ChristmasList.js
+++ src/components/ChristmasList.js
@@ -17,6 +17,7 @@ const ChristmasList = () => {
   return (
     <Box>
         <h1>Liste de cadeaux</h1>
+        <h2>------</h2>
         <ul>
             <li>Un parfum (deja pris)</li>
             <li>Livre The Watcher 2 <a href='https://livre.fnac.com/a12978611/The-Witcher-Sorceleur-Tome-2-Sorceleur-T2-L-Epee-de-la-providence-Andrzej-Sapkowski'>(celui-la par exemple)</a></li>
