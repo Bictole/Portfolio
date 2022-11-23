aee870db5007c740bc27301ee787f66653c46c8c
Victor Simonin <victor.simonin@epita.fr>
2022-11-23 20:26:54 +0100

    [Add] ChristmasList Ideas

--- src/components/ChristmasList.js
+++ src/components/ChristmasList.js
@@ -19,8 +19,9 @@ const ChristmasList = () => {
         <h1>Liste de cadeaux</h1>
         <h2>------</h2>
         <ul>
-            <li>Un parfum (deja pris)</li>
-            <li>Livre The Watcher 2 <a href='https://livre.fnac.com/a12978611/The-Witcher-Sorceleur-Tome-2-Sorceleur-T2-L-Epee-de-la-providence-Andrzej-Sapkowski'>(celui-la par exemple)</a></li>
+            <li>Un parfum (déja pris trop tard !)</li>
+            <li>Livre The Watcher 2, plutot pas la version poche <a href='https://livre.fnac.com/a12978611/The-Witcher-Sorceleur-Tome-2-Sorceleur-T2-L-Epee-de-la-providence-Andrzej-Sapkowski'>(celui-la par exemple)</a></li>
+            <li>N'ayant pas beaucoup d'idée, je vais ajouter des éléments dans cette liste dans les jours a venir !</li>
         </ul>
     </Box>
   )
