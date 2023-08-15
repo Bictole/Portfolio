93cc79fb0de9d23ffb737b1effac15b59f4edaf7
Victor Simonin <victor.simonin@epita.fr>
2023-08-16 00:55:07 +0200

    Adding images and fix few typo

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,11 +1,16 @@
-// work data
+import Chantier from '../assets/Images/webDev.jpg'
+import GoogleCodeJam from '../assets/Images/google_code_jam.png'
+import Knapsack from '../assets/Images/knapsack.png'
+import Odyh from '../assets/Images/Odyh.png'
+import OCR from '../assets/Images/OCR.png'
+import Hackathon from '../assets/Images/hackathon.jpg'
 
 export const Work = [
     {
         id:1,
         name:"Google Code Jam",
-        description:"The competition that google host online every year. Mostly done in python to solve challenging algorithmic puzzles against the clock.",
-        tags:["google","competition","algorithmic_puzzle"],
+        imgSrc: GoogleCodeJam,
+        description:"Participated in Google Code Jam, showcasing problem-solving and coding skills",
         
         demo:"https://github.com/Bictole/Google_Code_Jam",
         github:"https://github.com/Bictole/Google_Code_Jam"
@@ -13,8 +18,8 @@ export const Work = [
     {
         id:2,
         name:"Genetic-Knapsack",
-        description:"A funny way to solve the Knapsack problem efficiently by using some genetic algorithm method.",
-        tags:["python","genetic", "knapsack"],
+        imgSrc: Knapsack,
+        description:"Knapsack problem solved efficiently by using some genetic algorithm method",
         
         demo:"https://github.com/Bictole/Genetic-Knapsack",
         github:"https://github.com/Bictole/Genetic-Knapsack"
@@ -22,8 +27,8 @@ export const Work = [
     {
         id:3,
         name:"ODYH",
-        description:"A RPG 2D game achieved with C# and unity based on the universe of Epita (my IT school).",
-        tags:["C#","unity","rpg", "game"],
+        imgSrc: Odyh,
+        description:"A RPG 2D game achieved with C# and unity based on the universe of Epita",
         
         demo:"https://github.com/Bictole/ODYH",
         github:"https://github.com/Bictole/ODYH"
@@ -31,8 +36,8 @@ export const Work = [
     {
         id:4,
         name:"OCR",
-        description:"An optical character recognition made in C, with pre-processing, segmentation and a neural network to recognize character.",
-        tags:["C","optical","neural_network"],
+        imgSrc: OCR,
+        description:"An optical character recognition made in C with a simple neural network",
         
         demo:"https://github.com/Bictole/OCR",
         github:"https://github.com/Bictole/OCR"
@@ -40,8 +45,8 @@ export const Work = [
     },{
         id:5,
         name:"Hackathon 4.0",
-        description:"Small browser extension achieved during an Hackathon in India. The purpose was to help disabled people to access internet easily",
-        tags:["react","redux","framer-motion"],
+        imgSrc: Hackathon,
+        description:"Browser extension in an Indian Hackathon, aiding internet accessibility",
         
         demo:"https://github.com/Bictole/HACKATHON4.0",
         github:"https://github.com/Bictole/HACKATHON4.0"
